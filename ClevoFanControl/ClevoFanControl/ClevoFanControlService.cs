e1e0027ca199417723a08a8a7db2bebb38fb19d0
Nicolaj Kirchhof <nico@docoyo.com>
2018-06-11 01:09:36 +0200

    Added service template and powershell script

---
+++ ClevoFanControl/ClevoFanControl/ClevoFanControlService.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Timers;
+using GetCoreTempInfoNET;
+using Microsoft.Management.Infrastructure;
+
+namespace ClevoFanControl
+{
+    public partial class ClevoFanControlService : ServiceBase
+    {
+        private CoreTempInfo coreTempInfo;
+        CimInstance searchInstance = null;
+        private static readonly string clevoAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)+"/ClevoFanControl";
+        private static readonly string clevoLog = clevoAppData + "/ClevoFanControl.log";
+        Timer eventTimer;
+
+        private StreamWriter logWriter;
+        public ClevoFanControlService()
+        {
+            InitializeComponent();
+
+            //Initiate CoreTempInfo class.
+            coreTempInfo = new CoreTempInfo();
+            //Sign up for an event reporting errors
+            coreTempInfo.ReportError += new ErrorOccured(CTInfo_ReportError);
+
+
+            string Namespace = @"root\WMI";
+            string className = "CLEVO_GET";
+            CimInstance clevo = new CimInstance(className, Namespace);
+
+            CimSession mySession = CimSession.Create("localhost");
+            CimInstance searchInstance = mySession.GetInstance(Namespace, clevo);
+
+            // Set up a timer to trigger every minute.  
+            eventTimer = new Timer
+            {
+                Interval = 1000 // 60 seconds  
+            };
+            eventTimer.Elapsed += new ElapsedEventHandler(this.OnTimer);
+            eventTimer.Start();
+
+            if (!Directory.Exists(clevoAppData))
+            {
+                Directory.CreateDirectory(clevoAppData);
+            }
+
+        }
+
+        void CTInfo_ReportError(ErrorCodes ErrCode, string ErrMsg)
+        {
+            eventLog.WriteEntry(ErrMsg);
+        }
+
+        private void OnTimer(object sender, ElapsedEventArgs e)
+        {
+
+            //Attempt to read shared memory.
+            bool readSuccess = coreTempInfo.GetData();
+
+            //If read was successful the post the new info on the console.
+            if (readSuccess)
+            {
+                var temps = coreTempInfo.GetTemp;
+                var maxTemp = temps.Max();
+                logWriter.WriteLine("Tmax {0}", maxTemp);
+            }
+            else
+            {
+               eventLog.WriteEntry("Internal error name: " + coreTempInfo.GetLastError);
+               eventLog.WriteEntry("Internal error message: " + coreTempInfo.GetErrorMessage(coreTempInfo.GetLastError));
+            }
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog.WriteEntry("Starting clevo fan control.");
+            logWriter = new StreamWriter(clevoLog);
+            eventTimer.Start(); 
+        }
+
+        protected override void OnStop()
+        {
+            eventTimer.Stop();
+            logWriter.Close();
+        }
+    }
+}
---
+++ fan-control.ps1
@@ -0,0 +1,75 @@
+﻿$scriptpath = (split-path $MyInvocation.MyCommand.Path)
+
+Add-Type -LiteralPath "$scriptpath\GetCoreTempInfoNET.dll"
+Add-Type -AssemblyName PresentationFramework
+
+$ct = [GetCoreTempInfoNET.CoreTempInfo]::new()
+
+$clevo = get-wmiobject -query "select * from CLEVO_GET" -namespace "root\WMI"
+
+
+$steps = @(
+    (125,  80,  75,  70,  65,  60,  55,  50, 45, 0),
+    (255, 230, 200, 180, 155, 135, 128, 100, 64, 0)
+)
+$cpuLevel = 0
+$gpuLevel = 0
+$cpuTemp = 0
+$gpuTemp = 0
+$result = $clevo.SetFanAutoDuty(0)
+
+try{
+  while($true){
+      do{
+          sleep 1.5
+          if ($ct.GetData()){
+              $newCpuTemp = ($ct.GetTemp[0..3] | measure -Maximum).Maximum
+              $newGpuTemp = (get-wmiobject -query "SELECT * FROM Sensor WHERE InstanceId='3850'" -namespace "root\OpenHardwareMonitor").Value
+          } else {
+              $clevo.SetFanAutoDuty(1);
+              [System.Windows.MessageBox]::Show('Clevo Fan Control could not get temperature values, please restart.');
+              exit;
+          }
+      } while([math]::Abs($cpuTemp - $newCpuTemp) -le 3 -and [math]::Abs($gpuTemp - $newGpuTemp) -le 3)
+      Write-Output "$(Get-Date -Format o): newCpuTemp $newCpuTemp°C, temp $cpuTemp °C"
+      Write-Output "$(Get-Date -Format o): newGpuTemp $newGpuTemp°C, temp $gpuTemp °C"
+      $cpuTemp = $newCpuTemp
+      $gpuTemp = $newGpuTemp
+      $newCpuLevel = 0;
+      for(; $steps[0][$newCpuLevel] -gt $cpuTemp; $newCpuLevel++){}
+      Write-Output "$(Get-Date -Format o): New Cpu Level $newCpuLevel, Level $cpuLevel"
+      
+      if ($newCpuLevel -lt $cpuLevel -or $newCpuLevel -gt $cpuLevel+1){
+          Write-Output "$(Get-Date -Format o): Setting fan duty to $([int]($steps[1][$newCpuLevel]/255*100))% ($($steps[1][$newCpuLevel]))"
+          $cpuLevel = $newCpuLevel;
+      }
+
+       $newGpuLevel = 0;
+      for(; $steps[0][$newGpuLevel] -gt $gpuTemp; $newGpuLevel++){}
+      Write-Output "$(Get-Date -Format o): New Gpu Level $newGpuLevel, Level $gpuLevel"
+      
+      if ($newGpuLevel -lt $gpuLevel -or $newGpuLevel -gt $gpuLevel+1){
+          Write-Output "$(Get-Date -Format o): Setting fan duty to $([int]($steps[1][$newGpuLevel]/255*100))% ($($steps[1][$newGpuLevel]))"
+          $gpuLevel = $newGpuLevel;
+      }
+      $fanValue = ($steps[1][$newGpuLevel] * [uint32]"0x10000") + ($steps[1][$newGpuLevel] * [uint32]"0x0100") + $steps[1][$newCpuLevel]
+      Write-Output "New Fan value $("{0:x}" -f $fanValue)"
+      $result = $clevo.SetFanDuty($fanValue);
+      if ($result.Data1 -eq 104){
+          Write-Output "$(Get-Date -Format o): Successful applied new fan duty "
+      } else{
+          Write-Output "$(Get-Date -Format o): Failure applying new fan duty reverting to automatic"
+          $clevo.SetFanAutoDuty(1);
+          [System.Windows.MessageBox]::Show('Clevo Fan Control could not apply duty cycle, please restart.');
+          exit
+      }
+
+  }
+}
+catch {
+  $clevo.SetFanAutoDuty(1);
+  $ErrorMessage = $_.Exception.Message
+  $FailedItem = $_.Exception.ItemName
+   Write-Output "$(Get-Date -Format o): Unknown Failure. Item: $FailedItem, Message: $ErrorMessage"
+  [System.Windows.MessageBox]::Show('Clevo Fan Control had an unknown error, please restart'); 
+}
